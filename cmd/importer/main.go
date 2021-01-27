af7bb81cc556db0260b80c6af5746ecc1921feed
Vincent Poneleit <vincent.poneleit@idealo.de>
2021-01-27 20:07:26 +0100

    implemented mtg api request | added zerlog as logger

--- cmd/importer/main.go
+++ cmd/importer/main.go
@@ -1,9 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"github.com/rs/zerolog/log"
+
+	"net/http"
+)
+
 func main(){
+	response, err := ImportCardInfo()
+	if err != nil {
+		log.Err(err)
+	}
+	fmt.Println(response.StatusCode)
+	fmt.Println(response.Body)
 
 }
+/*
+ImportCardInfo receives a response with type *http.Response from
+the official mtg api containing all available card detail for one card
+specified with the multiverseID
+ */
+func ImportCardInfo() (*http.Response, error) {
+	URL := "https://api.magicthegathering.io/v1/cards/"
+	multiverseID := "386616"
 
-func ImportCardInfo(){
+	resp, err := http.Get(URL+multiverseID)
+	if err != nil {
+		log.Print(err)
+	}
 
+	defer func() {
+		err := resp.Body.Close()
+		if err != nil {
+			log.Print(err)
+		}
+	}()
+
+	return resp, err
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module magicTGArchive
 
 go 1.14
+
+require github.com/rs/zerolog v1.20.0 // indirect
