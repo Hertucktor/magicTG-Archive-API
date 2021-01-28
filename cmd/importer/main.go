e4bb3cbe666bfb40acd866efc300ddf280cc4490
Vincent Poneleit <vincent.poneleit@idealo.de>
2021-01-28 12:49:46 +0100

    added request handler

--- cmd/importer/main.go
+++ cmd/importer/main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"magicTGArchive/internal/pkg/importer"
-)
+func main() {
 
-const URL = "https://api.magicthegathering.io/v1/cards?name=Archangel Avacyn"
-
-func main(){
-	filter := ""
-
-	card,err := importer.RequestCardInfo(URL, filter)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Println(card)
 }
