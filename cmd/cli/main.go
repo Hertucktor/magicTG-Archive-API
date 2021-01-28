4f790eec5e7f2f87fd3aa29949cbba35ded0c26d
Vincent Poneleit <vincent.poneleit@idealo.de>
2021-01-28 18:57:49 +0100

    implemented cli

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,36 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"github.com/rs/zerolog/log"
-	"github.com/urfave/cli/v2"
-	"os"
+	"magicTGArchive/internal/pkg/cli"
 )
 
 func main(){
-	app := cli.NewApp()
-
-	app.Flags = []cli.Flag {
-		&cli.StringFlag{
-			Name: "lang",
-			Value: "english",
-			Usage: "language for the greeting",
-			Required: true,
-		},
-	}
-
-	app.Action = func(c *cli.Context) error {
-		var output string
-		if c.String("lang") == "de" {
-			output = "Moin"
-		} else {
-			output = "Hello"
-		}
-		fmt.Println(output)
-		return nil
-	}
-
-	err := app.Run(os.Args)
+	_, err := cli.ReadFromCLI()
 	if err != nil {
 		log.Fatal().Err(err)
 	}
