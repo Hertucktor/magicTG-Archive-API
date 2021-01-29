40d5626f940d3d79fc9338bbdd0b46c1840bb0c2
Vincent Poneleit <vincent.poneleit@idealo.de>
2021-01-29 17:09:25 +0100

    resturcturing main

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/rs/zerolog/log"
 	"magicTGArchive/internal/pkg/cli"
 	"magicTGArchive/internal/pkg/importer"
+	"magicTGArchive/internal/pkg/mongodb"
 )
 
 func main() {
@@ -15,28 +16,29 @@ func main() {
 	}
 
 	URL := importer.URLGenerator(language, cardName)
+
 	fmt.Println(URL)
+
 	cardInfo, err := importer.RequestCardInfo(URL)
 	if err != nil {
 		log.Fatal().Err(err)
 	}
 
-	for _, cards := range cardInfo.Cards{
+	/*for _, cards := range cardInfo.Cards{
 		for _, names := range cards.ForeignNames {
 			fmt.Println(names.Language)
 		}
-	}
-	/*
+	}*/
+
 	client, ctx, err := mongodb.CreateClient()
 	if err != nil {
 		log.Fatal().Err(err)
 	}
 
-
 	if err = mongodb.InsertCardInfo(cardInfo, client, ctx); err != nil {
 		log.Fatal().Err(err)
 	}
-
+	/*
 	if err = mongodb.GetAllCardInfo(client, ctx); err != nil{
 		log.Fatal().Err(err)
 	}
