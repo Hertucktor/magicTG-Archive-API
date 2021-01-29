4900ced28d99443cf47c3bccaf10352a7c3b9156
Vincent Poneleit <vincent.poneleit@idealo.de>
2021-01-29 15:36:57 +0100

    called cli func

--- cmd/main.go
+++ cmd/main.go
@@ -1,42 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"github.com/rs/zerolog/log"
-	"magicTGArchive/internal/pkg/mongodb"
+	"magicTGArchive/internal/pkg/cli"
 )
 
 func main() {
-	/*language, cardName, err := cli.ReadFromCLI()
+	language, cardName, err := cli.ReadFromCLI()
 	if err != nil {
 		log.Fatal().Err(err)
 	}
 
-	URL := importer.URLGenerator(language, cardName)
+	fmt.Println(language, cardName)
+
+	/*URL := importer.URLGenerator(language, cardName)
 
 	cardInfo, err := importer.RequestCardInfo(URL)
 	if err != nil {
 		log.Fatal().Err(err)
-	}*/
+	}
 
 	client, ctx, err := mongodb.CreateClient()
 	if err != nil {
 		log.Fatal().Err(err)
 	}
 
-	/*err = mongodb.InsertCardInfo(cardInfo, client, ctx)
+	err = mongodb.InsertCardInfo(cardInfo, client, ctx)
 	if err != nil {
-		log.Fatal().Err(err)
-	}*/
-
-	/*if err = mongodb.GetAllCardInfo(client, ctx); err != nil{
-		log.Fatal().Err(err)
-	}*/
-
-	if err = mongodb.GetFilteredSingleCardInfo(client, ctx); err != nil{
 		log.Fatal().Err(err)
 	}
 
-	//for _, card := range card.Cards{
-	//	fmt.Println(card.ImageURL)
-	//}
+	if err = mongodb.GetAllCardInfo(client, ctx); err != nil{
+		log.Fatal().Err(err)
+	}
+
+	if err = mongodb.GetFilteredSingleCardInfo(client, ctx); err != nil{
+		log.Fatal().Err(err)
+	}*/
 }
