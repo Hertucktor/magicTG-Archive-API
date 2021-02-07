fcd3de4680443251c8d4107a7b19f08140d20123
Vincent Poneleit <vincent.poneleit@idealo.de>
2021-02-07 20:23:56 +0100

    added env var handling

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
 	github.com/rs/zerolog v1.20.0
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/spf13/viper v1.7.1
 	github.com/urfave/cli/v2 v2.3.0
 	go.mongodb.org/mongo-driver v1.4.5
 	golang.org/x/crypto v0.0.0-20190605123033-f99c8df09eb5 // indirect
--- internal/pkg/env/handler.go
+++ internal/pkg/env/handler.go
@@ -1,5 +1,10 @@
 package env
 
+import (
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
+)
+
 type Conf struct {
 	dbUser string
 	dbPass string
@@ -7,6 +12,21 @@ type Conf struct {
 	dbName string
 }
 
-func ReceiveEnvVars(){
+func ReceiveEnvVars() (Conf, error) {
+	var err error
+	viper.SetConfigFile(".env")
 
+	if err := viper.ReadInConfig(); err != nil {
+		log.Error().Timestamp().Err(err).Msg("Error: loading env vars")
+		return Conf{}, err
+	}
+
+	conf := Conf{
+		dbUser: viper.Get("DB_USER").(string),
+		dbPass: viper.Get("DB_PASS").(string),
+		dbPort: viper.Get("DB_PORT").(string),
+		dbName: viper.Get("DB_NAME").(string),
+	}
+
+	return conf, err
 }
