3564e039cc6b00d1523b1caa446cad6a8392cbef
Vincent Poneleit <vincent.poneleit@idealo.de>
2021-02-07 17:15:39 +0100

    return client cancelFunc

--- internal/pkg/mongodb/client.go
+++ internal/pkg/mongodb/client.go
@@ -8,20 +8,20 @@ import (
 	"time"
 )
 
-func CreateClient() (*mongo.Client, context.Context, error) {
+func CreateClient() (*mongo.Client, context.Context, context.CancelFunc, error) {
 	//TODO: secure username and password
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://admin:admin@127.0.0.1:27017/Magic:The-Gathering-Archive"))
 	if err != nil {
 		log.Error().Err(err)
-		return client, nil, err
+		return client,nil, nil, err
 	}
 
-	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 30*time.Second)
 	err = client.Connect(ctx)
 	if err != nil {
 		log.Error().Err(err)
-		return client, ctx, err
+		return client, ctx, cancelFunc, err
 	}
 
-	return client, ctx, err
+	return client, ctx, cancelFunc, err
 }
