46921e48a294b82a0c001fbdb520ef96ae06daf1
Vincent Poneleit <vincent.poneleit@idealo.de>
2021-02-07 16:27:29 +0100

    optimzed logging

--- internal/pkg/database/handler.go
+++ internal/pkg/database/handler.go
@@ -10,18 +10,18 @@ func InsertDataset(cardInfo importer.APIResponseForOneCard) error {
 
 	card, err := mongodb.SingleCardInfo(cardInfo.Card.Name)
 	if err != nil {
-		log.Error().Err(err)
+		log.Error().Timestamp().Err(err).Msg("Error reading from the database")
 		return err
 	}
 
 	if card.ID != "" {
 		if err := mongodb.UpdateSingleCard(cardInfo.Card.Name, card.Quantity); err != nil {
-			log.Error().Err(err)
+			log.Error().Timestamp().Err(err).Msg("Error updating the database")
 			return err
 		}
 	} else {
 		if err := mongodb.InsertCard(cardInfo.Card); err != nil {
-			log.Error().Err(err)
+			log.Error().Timestamp().Err(err).Msg("Error inserting in the database")
 			return err
 		}
 	}
