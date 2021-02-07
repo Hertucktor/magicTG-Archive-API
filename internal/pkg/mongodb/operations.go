0e2dcbf749ca3ac4e28fe4b79d9d499431107662
Vincent Poneleit <vincent.poneleit@idealo.de>
2021-02-07 17:12:06 +0100

    improved logging

--- internal/pkg/mongodb/operations.go
+++ internal/pkg/mongodb/operations.go
@@ -14,61 +14,65 @@ func InsertCard(cardInfo importer.Card) error {
 
 	client, ctx, err := CreateClient()
 	if err != nil {
-		log.Error().Err(err)
+		log.Error().Timestamp().Err(err).Msg("Error: Creating Client\n")
 		return err
 	}
 
 	defer func() {
 		if err := client.Disconnect(ctx); err != nil {
-			log.Err(err)
+			log.Error().Timestamp().Err(err).Msg("Error: closing client\n")
 		}
 	}()
 
 	collection := client.Database(dbName).Collection(dbCollection)
+	log.Info().Timestamp().Msgf("Successful: created collection:\n", collection)
 
 	insertResult, err := collection.InsertOne(ctx, cardInfo)
 	if err != nil {
-		log.Error().Err(err)
+		log.Error().Timestamp().Err(err).Msgf("Error: couldn't insert into collection of db:\n", dbCollection, dbName)
 		return err
 	}
 
-	log.Info().Msgf("",insertResult.InsertedID)
+	log.Info().Msgf("Success: insertion result:\n", insertResult)
 
 	return err
 }
 
 func AllCardInfo() (bson.M, error){
+	var filter = bson.M{}
 	var cards bson.M
 
 	client, ctx, err := CreateClient()
 	if err != nil {
-		log.Error().Err(err)
+		log.Error().Timestamp().Err(err).Msg("Error: Creating Client\n")
 		return cards, err
 	}
 
 	defer func() {
 		if err := client.Disconnect(ctx); err != nil {
-			log.Err(err)
+			log.Error().Timestamp().Err(err).Msg("Error: closing client\n")
 		}
 	}()
 
 	collection := client.Database(dbName).Collection(dbCollection)
+	log.Info().Timestamp().Msgf("Successful: created collection:\n", collection)
 
-	cursor, err := collection.Find(ctx, bson.M{})
+	cursor, err := collection.Find(ctx, filter)
 	if err != nil {
-		log.Error().Err(err)
+		log.Error().Timestamp().Err(err).Msgf("Error: ")
 		return cards, err
 	}
 
 	defer func() {
 		if err := cursor.Close(ctx); err != nil {
-			log.Error().Err(err)
+			log.Error().Timestamp().Err(err).Msgf("Error: couldn't close cursor:\n", cursor)
 		}
+		log.Info().Msgf("Closed cursor:", cursor)
 	}()
 
 	for cursor.Next(ctx) {
 		if err = cursor.Decode(&cards); err != nil {
-			log.Error().Err(err)
+			log.Error().Timestamp().Err(err).Msgf("Error: couldn't decode data into interface:\n", cards)
 			return cards, err
 		}
 	}
@@ -79,29 +83,37 @@ func AllCardInfo() (bson.M, error){
 func SingleCardInfo(cardName string) (DBCard, error) {
 	var cardInfoFiltered []DBCard
 	var singleCard DBCard
-	filter := bson.M{"name": cardName}
+	var filter = bson.M{"name": cardName}
 
 	client, ctx, err := CreateClient()
 	if err != nil {
-		log.Error().Err(err)
+		log.Error().Timestamp().Err(err).Msg("Error: Creating Client\n")
 	}
 
 	defer func() {
 		if err := client.Disconnect(ctx); err != nil {
-			log.Err(err)
+			log.Error().Timestamp().Err(err).Msg("Error: closing client\n")
 		}
 	}()
 
 	collection := client.Database(dbName).Collection(dbName)
+	log.Info().Timestamp().Msgf("Success: created collection:\n", collection)
 
-	filterCursor, err := collection.Find(ctx, filter)
+	cursor, err := collection.Find(ctx, filter)
 	if err != nil {
-		log.Error().Err(err)
+		log.Error().Timestamp().Err(err).Msg("Error: cursor couldn't be created\n")
 		return singleCard, err
 	}
 
-	if err = filterCursor.All(ctx, &cardInfoFiltered); err != nil {
-		log.Error().Err(err)
+	defer func() {
+		if err := cursor.Close(ctx); err != nil {
+			log.Error().Timestamp().Err(err).Msgf("Error: couldn't close cursor", cursor)
+		}
+		log.Info().Msg("Success: Closed cursor\n")
+	}()
+
+	if err = cursor.All(ctx, &cardInfoFiltered); err != nil {
+		log.Error().Timestamp().Err(err).Msg("Error: problem with the cursor\n")
 		return singleCard, err
 	}
 
@@ -113,60 +125,63 @@ func SingleCardInfo(cardName string) (DBCard, error) {
 }
 
 func DeleteSingleCard(cardName string) error {
-	client, ctx, err := CreateClient()
-	if err != nil {
-		log.Error().Err(err)
-		return err
-	}
-
-	defer func() {
-		err := client.Disconnect(ctx)
-		log.Err(err)
-	}()
-
-	collection := client.Database(dbName).Collection(dbCollection)
-
-	result, err := collection.DeleteOne(ctx, bson.M{"name": cardName})
-	if err != nil {
-		log.Error().Err(err)
-		return err
-	}
-
-	log.Info().Msgf("Amount of Documents removed:\n", result.DeletedCount)
-
-	return err
-}
-
-func UpdateSingleCard(cardName string, cardQuantity int) error {
-	newQuantity := cardQuantity+1
+	var filter = bson.M{"name": cardName}
 
 	client, ctx, err := CreateClient()
 	if err != nil {
-		log.Error().Err(err)
+		log.Error().Timestamp().Err(err).Msg("Error: Creating Client\n")
 		return err
 	}
 
 	defer func() {
 		if err := client.Disconnect(ctx); err != nil {
-			log.Err(err)
+			log.Error().Timestamp().Err(err).Msg("Error: closing client\n")
 		}
 	}()
 
 	collection := client.Database(dbName).Collection(dbCollection)
+	log.Info().Timestamp().Msgf("Success: created collection:\n", collection)
 
-	result, err := collection.UpdateOne(
-		ctx,
-		bson.M{"name": cardName},
-		bson.D{
-			{"$set", bson.D{{"quantity", newQuantity}}},
-		},
-	)
+	deleteResult, err := collection.DeleteOne(ctx, filter)
 	if err != nil {
-		log.Error().Err(err)
+		log.Error().Timestamp().Err(err).Msgf("Error: couldn't delete document with given filter\n",filter)
 		return err
 	}
 
-	log.Info().Msgf("Updated %v Documents!\n", result.ModifiedCount)
+	log.Info().Timestamp().Msgf("Success: Result after successful deletion:\n", deleteResult)
+
+	return err
+}
+
+func UpdateSingleCard(cardName string, cardQuantity int) error {
+	var newQuantity = cardQuantity+1
+	var updateFilter = bson.M{"name": cardName}
+	var updateSet = bson.D{
+		{"$set", bson.D{{"quantity", newQuantity}}},
+	}
+
+	client, ctx, err := CreateClient()
+	if err != nil {
+		log.Error().Timestamp().Err(err).Msg("Error: Creating client\n")
+		return err
+	}
+
+	defer func() {
+		if err := client.Disconnect(ctx); err != nil {
+			log.Error().Timestamp().Err(err).Msg("Error: closing client\n")
+		}
+	}()
+
+	collection := client.Database(dbName).Collection(dbCollection)
+	log.Info().Timestamp().Msgf("Success: created collection:\n", collection)
+
+	updateResult, err := collection.UpdateOne(ctx, updateFilter, updateSet)
+	if err != nil {
+		log.Error().Timestamp().Err(err).Msgf("Error: updating the quantity of a card in collection of db:\n", dbCollection, dbName)
+		return err
+	}
+
+	log.Info().Timestamp().Msgf("Success: Updated Documents!\n", updateResult)
 
 	return err
 }
