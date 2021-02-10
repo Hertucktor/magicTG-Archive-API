8f315484ae1f940678b2ac02f1407047318660d0
Vincent Poneleit <vincent.poneleit@idealo.de>
2021-02-10 20:03:46 +0100

    importer v1.0

---
+++ cmd/importer/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/rs/zerolog/log"
+	"magicTGArchive/internal/pkg/importer"
+	"magicTGArchive/internal/pkg/mongodb"
+	"strconv"
+)
+
+func main() {
+	var page int
+	var delimiter = 1
+	var collectionName = "allCards"
+
+	for delimiter != 0{
+		resp, err := importer.RequestAllCards(strconv.Itoa(page))
+		if err != nil {
+			log.Fatal().Timestamp().Err(err).Msg("Fatal: API Request threw error")
+		}
+
+		for _, card := range resp.Cards{
+			if err := mongodb.InsertCard(card, collectionName); err != nil {
+				log.Fatal().Timestamp().Err(err).Msgf("Error: couldn't insert dataset:\n",card)
+			}
+		}
+		page++
+		fmt.Println(page)
+		delimiter = len(resp.Cards)
+	}
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/rs/zerolog/log"
-	"magicTGArchive/internal/pkg/importer"
-	"magicTGArchive/internal/pkg/mongodb"
-	"strconv"
-)
-
 func main() {
 	/*var URL string
 	var multiverseID string
@@ -41,25 +33,5 @@ func main() {
 		log.Fatal().Err(err)
 
 	}*/
-	var page int
-	var delimiter = 1
-	var collectionName = "allCards"
-
-	for delimiter != 0{
-		resp, err := importer.RequestAllCards(strconv.Itoa(page))
-		if err != nil {
-			log.Fatal().Timestamp().Err(err).Msg("Fatal: API Request threw error")
-		}
-
-		for _, card := range resp.Cards{
-			if err := mongodb.InsertCard(card, collectionName); err != nil {
-				log.Fatal().Timestamp().Err(err).Msgf("Error: couldn't insert dataset:\n",card)
-			}
-		}
-		page++
-		fmt.Println(page)
-		delimiter = len(resp.Cards)
-	}
-
 
 }
