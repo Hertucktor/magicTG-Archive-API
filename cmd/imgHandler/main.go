b4c2a5f5a3132f2b3ebbec9aab94feb6d726cd80
Vincent Poneleit <vincent.poneleit@idealo.de>
2021-02-20 05:12:07 +0100

    trying to get csv to work

--- cmd/imgHandler/main.go
+++ cmd/imgHandler/main.go
@@ -1,22 +1,49 @@
 package main
 
 import (
+	"encoding/csv"
+	"fmt"
 	"github.com/rs/zerolog/log"
-	"magicTGArchive/internal/pkg/imgHandler"
+	"io"
+	"os"
 )
 
-var dbCollName = "setImages"
+//var dbCollName = "setImages"
 
 func main() {
-	var img imgHandler.Img
+	/*var img imgHandler.Img
 	//var imgBaseURL = "https://media.magic.wizards.com/images/featured/"
-
 
 	if err := imgHandler.InsertSetImg(img, dbCollName); err != nil {
 		log.Fatal().Timestamp().Err(err).Msg("Fatal: couldn't insert ImgData into db")
-	}
+	}*/
+
+
+	ReadCSV()
+
 }
 
-func readCSV(){
+func ReadCSV(){
+	fmt.Println("s")
+	csvFile, err := os.Open("./csv/mtgSetIcons.csv")
+	fmt.Println(csvFile)
+	if err != nil {
+		log.Error().Timestamp().Err(err).Msg("Error: couldn't open CSV")
+	}
+
+	r := csv.NewReader(csvFile)
+	fmt.Println(r)
+	end := false
+	for end == true  {
+		record, err := r.Read()
+		if err == io.EOF{
+			end = true
+		}
+		if err != nil {
+			log.Fatal().Timestamp().Err(err)
+		}
+
+		fmt.Printf("Question: %s Answer %s\n", record[0], record[1])
+	}
 
 }
