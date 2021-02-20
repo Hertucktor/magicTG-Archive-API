a767a294b6e3e5e6f68edf16230d69e79a043723
Vincent Poneleit <vincent.poneleit@idealo.de>
2021-02-20 03:30:18 +0100

    added comments | simplified ifs

--- internal/pkg/importer/request.go
+++ internal/pkg/importer/request.go
@@ -10,14 +10,16 @@ import (
 
 /*
 RequestAllCards receives a response with type *http.Response from
-the mtg api containing 100 cards
+the mtg api containing 100 cards.
 Returning the response and an error
 */
 func RequestAllCards(page string) (APIResponseForMultipleCards, error) {
 	var response APIResponseForMultipleCards
-
-	resp, err := http.Get("https://api.magicthegathering.io/v1/cards?page="+page)
-	if err != nil {
+	var resp *http.Response
+	var err error
+	var body []byte
+	//GET request to URL with page param
+	if resp, err = http.Get("https://api.magicthegathering.io/v1/cards?page="+page); err != nil{
 		log.Error().Timestamp().Err(err).Msg("Error: problem with http GET request\n")
 		return response, err
 	}
@@ -25,26 +27,23 @@ func RequestAllCards(page string) (APIResponseForMultipleCards, error) {
 	log.Info().Timestamp().Msgf("HTTP GET REQUEST TO https://api.magicthegathering.io/v1/cards?page=\n",page)
 
 	defer func() {
-		err := resp.Body.Close()
-		if err != nil {
-			log.Error().Timestamp().Err(err).Msg("Error: could't close response body\n")
+		if err = resp.Body.Close(); err != nil {
+			log.Fatal().Timestamp().Err(err).Msg("Fatal: couldn't close response body\n")
 		}
 	}()
-
+	//checks if there is an http status code other than 200 in the response
 	if resp.StatusCode != 200{
-		err := errors.New("Http statuscode != 200")
+		err = errors.New("Http statuscode != 200")
 		log.Error().Timestamp().Err(err).Msgf("Error: Http status code:\n", resp.StatusCode)
 		return response, err
 	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
+	//reads response body into []byte
+	if body, err = ioutil.ReadAll(resp.Body); err != nil {
 		log.Error().Timestamp().Err(err).Msg("Error: couldn't read from response body\n")
 		return response, err
 	}
-
-	err = json.Unmarshal(body, &response)
-	if err != nil {
+	//parses response body []byte values into response
+	if err = json.Unmarshal(body, &response); err != nil {
 		log.Error().Timestamp().Err(err).Msg("Error: couldn't unmarshal body into MTGDevAPIResponse struct\n")
 		return response, err
 	}
