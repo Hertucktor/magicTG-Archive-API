c7989af4ef9eea5fe2e792ef4d9ea39a5b06df67
Vincent Poneleit <vincent.poneleit@idealo.de>
2021-02-28 21:03:41 +0100

    trying ajax in vanilla js | still not working

--- static/js/selection.js
+++ static/js/selection.js
@@ -7,11 +7,20 @@ function receiveSelected() {
 function requestCardsBySetName(setName) {
     let apiEndpoint = "localhost:8080/api/cards/set-names/"
     let URL = `${apiEndpoint + setName}`;
-    alert(URL);
-    /*$(document).ready(function() {
-        $.get(URL, function(data, status){
-            //alert("Data: " + data + "\nStatus: " + status);
-            console.log(status)
-        });
-    });*/
+    console.log("request")
+
+    loadCards(URL)
+}
+
+function loadCards(URL) {
+    var xhttp = new XMLHttpRequest();
+    console.log(xhttp.readyState)
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(this.readyState)
+            console.log(this.responseText);
+        }
+    };
+    xhttp.open("GET", URL, true);
+    xhttp.send();
 }
