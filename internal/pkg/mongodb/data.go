8f0b7fc741cb3f42f38a8e7a50be97a12694f4c1
Vincent Poneleit <vincent.poneleit@idealo.de>
2021-03-02 22:07:27 +0100

    struct for cards updated

--- internal/pkg/mongodb/data.go
+++ internal/pkg/mongodb/data.go
@@ -1,19 +1,18 @@
 package mongodb
 
 type Card struct {
-	ID			  string 		 `json:"id,omitempty" bson:"_id,omitempty"`
 	Name          string         `json:"name,omitempty" bson:"name,omitempty"`
-	ManaCost      string         `json:"manaCost,omitempty" bson:"manacost,omitempty"`
+	ManaCost      string         `json:"manaCost,omitempty" bson:"manaCost,omitempty"`
 	Cmc           float64        `json:"cmc,omitempty" bson:"cmc,omitempty"`
 	Colors        []string       `json:"colors,omitempty" bson:"colors,omitempty"`
-	ColorIdentity []string       `json:"colorIdentity,omitempty" bson:"coloridentity,omitempty"`
+	ColorIdentity []string       `json:"colorIdentity,omitempty" bson:"colorIdentity,omitempty"`
 	Type          string         `json:"type,omitempty" bson:"type,omitempty"`
-	Supertypes    []string  	 `json:"supertypes,omitempty" bson:"supertypes,omitempty"`
+	Supertypes    []string  	 `json:"superTypes,omitempty" bson:"superTypes,omitempty"`
 	Types         []string       `json:"types,omitempty" bson:"types,omitempty"`
-	Subtypes      []string       `json:"subtypes,omitempty" bson:"subtypes,omitempty"`
+	Subtypes      []string       `json:"subTypes,omitempty" bson:"subTypes,omitempty"`
 	Rarity        string         `json:"rarity,omitempty" bson:"rarity,omitempty"`
 	Set           string         `json:"set,omitempty" bson:"set,omitempty"`
-	SetName       string         `json:"setname,omitempty" bson:"setname,omitempty"`
+	SetName       string         `json:"setName,omitempty" bson:"setName,omitempty"`
 	Text          string         `json:"text,omitempty" bson:"text,omitempty"`
 	Flavor        string         `json:"flavor,omitempty" bson:"flavor,omitempty"`
 	Artist        string         `json:"artist,omitempty" bson:"artist,omitempty"`
@@ -21,30 +20,33 @@ type Card struct {
 	Power         string         `json:"power,omitempty" bson:"power,omitempty"`
 	Toughness     string         `json:"toughness,omitempty" bson:"toughness,omitempty"`
 	Layout        string         `json:"layout,omitempty" bson:"layout,omitempty"`
-	Multiverseid  int            `json:"multiverseid,omitempty" bson:"multiverseid,omitempty"`
-	ImageURL      string         `json:"imageUrl,omitempty" bson:"imageurl,omitempty"`
-	Rulings       []Rulings  	 `json:"rulings,omitempty" bson:"rulings,omitempty"`
-	ForeignNames  []ForeignNames `json:"foreignNames,omitempty" bson:"foreignnames,omitempty"`
+	Multiverseid  string         `json:"multiverseID,omitempty" bson:"multiverseID,omitempty"`
+	ImageURL      string         `json:"imageURL,omitempty" bson:"imageURL,omitempty"`
+	Rulings       []Rulings      `json:"rulings,omitempty" bson:"rulings,omitempty"`
+	ForeignNames  []ForeignNames `json:"foreignNames,omitempty" bson:"foreignNames,omitempty"`
 	Printings     []string       `json:"printings,omitempty" bson:"printings,omitempty"`
-	OriginalText  string         `json:"originalText,omitempty" bson:"originaltext,omitempty"`
-	OriginalType  string         `json:"originalType,omitempty" bson:"originaltype,omitempty"`
+	OriginalText  string         `json:"originalText,omitempty" bson:"originalText,omitempty"`
+	OriginalType  string         `json:"originalType,omitempty" bson:"originalType,omitempty"`
 	Legalities    []Legalities   `json:"legalities,omitempty" bson:"legalities,omitempty"`
-	CardID        string         `json:"cardID,omitempty" bson:"id,omitempty"`
-	Quantity	  int 			 `json:"quantity" bson:"quantity"`
+	ID            string         `json:"id,omitempty" bson:"id,omitempty"`
+	Variations    []string       `json:"variations,omitempty" bson:"variations,omitempty"`
+	Quantity	  int            `json:"quantity,omitempty" bson:"quantity,omitempty"`
+	Created		  string         `json:"created,omitempty" bson:"created,omitempty"`
+	Modified      string         `json:"modified,omitempty" bson:"modified,omitempty"`
 }
 type Rulings struct {
-	Date 		string 			 `json:"date,omitempty" bson:"date"`
-	Text 		string 			 `json:"text,omitempty" bson:"text"`
+	Date string `json:"date,omitempty" bson:"date,omitempty"`
+	Text string `json:"text,omitempty" bson:"text,omitempty"`
 }
 type ForeignNames struct {
-	Name         string 		 `json:"name,omitempty" bson:"name"`
-	Text         string 		 `json:"text,omitempty" bson:"text"`
-	Flavor       string 		 `json:"flavor,omitempty" bson:"flavor"`
-	ImageURL     string 		 `json:"imageUrl,omitempty" bson:"imageurl"`
-	Language     string 		 `json:"language,omitempty" bson:"language"`
-	Multiverseid int    		 `json:"multiverseid,omitempty" bson:"multiverseid"`
+	Name         string `json:"name,omitempty" bson:"name,omitempty"`
+	Text         string `json:"text,omitempty" bson:"text,omitempty"`
+	Flavor       string `json:"flavor,omitempty" bson:"flavor,omitempty"`
+	ImageURL     string `json:"imageURL,omitempty" bson:"imageURL,omitempty"`
+	Language     string `json:"language,omitempty" bson:"language,omitempty"`
+	Multiverseid int    `json:"multiverseID,omitempty" bson:"multiverseID,omitempty"`
 }
 type Legalities struct {
-	Format   	string 			 `json:"format,omitempty" bson:"format"`
-	Legality 	string 			 `json:"legality,omitempty" bson:"legality"`
+	Format   string `json:"format,omitempty" bson:"format,omitempty"`
+	Legality string `json:"legality,omitempty" bson:"legality,omitempty"`
 }
