4327158dc3ab5d72a54a23d4db9800e0cd18c23b
Vincent Poneleit <vincent.poneleit@idealo.de>
2021-03-03 18:06:02 +0100

    error handling and params

--- cmd/imageComparator/main.go
+++ cmd/imageComparator/main.go
@@ -2,18 +2,29 @@ package main
 
 import (
 	"fmt"
+	"github.com/rs/zerolog/log"
 	"github.com/vitali-fedulov/images"
 )
 
 func main() {
-	// Open photos.
-	imgA, err := images.Open("./temp/images/compare/ModernHorizons.png")
-	if err != nil {
-		panic(err)
+	uploadedImgPath := "./temp/images/upload/ModernHorizons.png"
+	compareImgPath := "./temp/images/compare/Core20.png"
+
+
+	if err := selectImgToCompare(uploadedImgPath, compareImgPath); err != nil {
+		log.Fatal().Timestamp().Err(err).Msg("Fatal: couldn't compare images")
 	}
-	imgB, err := images.Open("./temp/images/compare/Core20.png")
+}
+
+func selectImgToCompare(uploadedImg string, compareImg string) error{
+	// Open photos.
+	imgA, err := images.Open(uploadedImg)
 	if err != nil {
-		panic(err)
+		log.Error().Timestamp().Err(err)
+	}
+	imgB, err := images.Open(compareImg)
+	if err != nil {
+		log.Error().Timestamp().Err(err)
 	}
 
 	// Calculate hashes and image sizes.
@@ -26,4 +37,6 @@ func main() {
 	} else {
 		fmt.Println("Images are distinct.")
 	}
+
+	return nil
 }
