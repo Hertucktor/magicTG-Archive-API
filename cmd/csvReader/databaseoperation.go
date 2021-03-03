827ef2ec8518032374c652e8683e04987d2f377b
Vincent Poneleit <vincent.poneleit@idealo.de>
2021-03-03 18:10:34 +0100

    insert func in own file

---
+++ cmd/csvReader/databaseoperation.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/rs/zerolog/log"
+	"magicTGArchive/internal/pkg/env"
+	"magicTGArchive/internal/pkg/mongodb"
+)
+
+func InsertImgInfo(imgInfo Img, dbCollection string) error {
+	conf, err := env.ReceiveEnvVars()
+	if err != nil {
+		log.Error().Timestamp().Err(err).Msg("Error: couldn't receive env vars")
+		return err
+	}
+
+	client, ctx, cancelCtx, err := mongodb.CreateClient()
+	if err != nil {
+		log.Error().Timestamp().Err(err).Msg("Error: Creating Client\n")
+		return err
+	}
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			log.Error().Timestamp().Err(err).Msg("Error: closing client\n")
+		}
+		cancelCtx()
+	}()
+
+	collection := client.Database(conf.DbName).Collection(dbCollection)
+	log.Info().Timestamp().Msgf("Successful: created collection:\n", collection)
+
+	insertResult, err := collection.InsertOne(ctx, imgInfo)
+	if err != nil {
+		log.Error().Timestamp().Err(err).Msgf("Error: couldn't insert into collection of db:\n", dbCollection, conf.DbName)
+		return err
+	}
+
+	log.Info().Msgf("Success: insertion result:\n", insertResult)
+
+	return err
+}
