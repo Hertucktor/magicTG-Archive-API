8f53105d841b9557ccb67d870d53bec5a686cf31
Vincent Poneleit <vincent.poneleit@idealo.de>
2021-03-04 22:14:46 +0100

    endpoint to receive all set names by array

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"github.com/rs/zerolog/log"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"magicTGArchive/internal/pkg/env"
+	"magicTGArchive/internal/pkg/mongodb"
 	"net/http"
 )
 
@@ -27,6 +34,7 @@ func setupRoutes(){
 	api.HandleFunc("/cards/collector-numbers/{number}/set-names/{setName}", returnSingleCardEntry).Methods(http.MethodGet) //From myCards Coll
 	api.HandleFunc("/cards/collector-number/{number}/set-names/{setName}", updateSingleCardEntry).Methods(http.MethodPut)
 	api.HandleFunc("/cards/collector-number/{number}/set-names/{setName}", deleteSingleCardEntry).Methods(http.MethodDelete)
+	api.HandleFunc("/cards/set-names", returnAllSetName).Methods(http.MethodGet)
 
 	//API Operations for img info
 	img := myRouter.PathPrefix("/img").Subrouter()
@@ -44,6 +52,109 @@ func setupRoutes(){
 	if err := http.ListenAndServe(port, myRouter); err != nil {
 		log.Panic().Timestamp().Err(err).Msg("Panic: problem with TCP network connection")
 	}
+}
+
+func returnAllSetName(w http.ResponseWriter, r *http.Request){
+	log.Info().Msg("Endpoint Hit: returnAllCardsBySet")
+
+	conf, err := env.ReceiveEnvVars()
+	if err != nil {
+		log.Fatal().Timestamp().Err(err).Msg("Fatal: couldn't receive env vars")
+	}
+
+	client, ctx, err := buildClient(conf)
+	if err != nil {
+		log.Fatal().Timestamp().Err(err).Msg("v: couldn't build client")
+	}
+
+	setNames, err := allSetNames(client, ctx, conf)
+	if err != nil {
+		w.WriteHeader(500)
+		_,_ = w.Write([]byte("set names couldn't been found"))
+		log.Error().Timestamp().Err(err).Msg("Error: couldn't return set names")
+		return
+	}
+
+	setNamesBytes, err := json.Marshal(setNames)
+	if err != nil {
+		log.Error().Timestamp().Err(err)
+		w.WriteHeader(500)
+		return
+	}
+
+	if _,err = w.Write(setNamesBytes); err != nil {
+		log.Error().Timestamp().Err(err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(200)
+
+}
+
+func allSetNames(client *mongo.Client, ctx context.Context, conf env.Conf)([]string, error){
+	var filter = bson.M{}
+	var cards []mongodb.Card
+
+	collection := client.Database(conf.DbName).Collection(conf.DbCollAllCards)
+	log.Info().Timestamp().Msgf("Successful: created collection:\n", collection)
+
+	cursor, err := collection.Find(ctx, filter)
+	if err != nil {
+		log.Error().Timestamp().Err(err).Msgf("Error: ")
+		return nil, err
+	}
+
+	if err = cursor.All(ctx, &cards); err != nil {
+		log.Error().Timestamp().Err(err).Msgf("Error: couldn't decode data into interface:\n")
+		return nil, err
+	}
+
+	setNames := sortSetNames(cards)
+
+	defer func() {
+		if err = cursor.Close(ctx); err != nil {
+			log.Error().Timestamp().Err(err).Msgf("Error: couldn't close cursor:%v", cursor.Current)
+		}
+		log.Info().Timestamp().Msg("Gracefully closed cursor")
+	}()
+
+	return setNames, err
+}
+
+func buildClient(conf env.Conf)(*mongo.Client, context.Context, error){
+
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://"+conf.DbUser+":"+conf.DbPass+"@"+conf.DbPort+"/"+conf.DbName))
+	if err != nil {
+		log.Error().Err(err)
+		return client, nil, err
+	}
+
+	ctx := context.TODO()
+	if err = client.Connect(ctx); err != nil {
+		log.Error().Err(err)
+		return client, ctx, err
+	}
+
+	return client, ctx, err
+}
+
+func sortSetNames(cards []mongodb.Card) ([]string){
+	var setNames []string
+	for _, card := range cards {
+		if !stringInSlice(card.SetName, setNames) {
+			setNames = append(setNames, card.SetName)
+		}
+	}
+	return setNames
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
 }
 
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
